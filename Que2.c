03558bbad3b2e431edd871f6c86c1131eff2c564
Dikshita621 <dikshitachoudhary845@gmail.com>
2025-09-08 10:01:05 +0530

    Create Que2.c

---
+++ Que2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("University of Petroleum and Energy Studies\n");
+    printf("P.O. Bidholi Via-Prem Nagar,\n");
+    printf("Dehradun - 248007\n");
+    printf("Uttarakhand, India\n");
+    return 0;
+}
