da1455f08d952796191578cc4396937fc72a666c
Dikshita621 <dikshitachoudhary845@gmail.com>
2025-09-19 12:36:28 +0530

    Create Que10.c

---
+++ Que10.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    int countPositive = 0, countNegative = 0, countZero = 0;
+    char choice;
+
+    do {
+        printf("Enter a number: ");
+        scanf("%d", &num);
+
+        if (num > 0) {
+            countPositive++;
+        } else if (num < 0) {
+            countNegative++;
+        } else {
+            countZero++;
+        }
+
+        printf("Do you want to enter another number? (y/n): ");
+        scanf(" %c", &choice);  // space before %c to consume any leftover newline character
+    } while (choice == 'y' || choice == 'Y');
+
+    printf("\nCount of Positive numbers: %d\n", countPositive);
+    printf("Count of Negative numbers: %d\n", countNegative);
+    printf("Count of Zeroes: %d\n", countZero);
+
+    return 0;
+}
