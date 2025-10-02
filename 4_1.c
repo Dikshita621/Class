83032794b0b6b25fe96610cf165fc8a047beac04
Dikshita621 <dikshitachoudhary845@gmail.com>
2025-10-02 15:37:05 +0530

    Update 4_1.c

--- 4_1.c
+++ 4_1.c
@@ -1,15 +1,13 @@
 /*Declare a global variable outside all functions and use it inside various functions to 
 understand its accessibility.*/
 #include <stdio.h>
-
 // Declare a global variable
 float globalVar;
 
 void setGlobalVar() {
     printf("Enter a value for the global variable: ");
     scanf("%f", &globalVar);  // Taking input into the global variable
-}
-
+} 
 void addValue(float value) {
     globalVar += value;
     printf("After adding %.2f, global variable: %.2f\n", value, globalVar);
@@ -19,21 +17,6 @@ void multiplyValue(float value) {
     globalVar *= value;
     printf("After multiplying by %.2f, global variable: %.2f\n", value, globalVar);
 }
-
-void divideValue(float value) {
-    if (value != 0) {
-        globalVar /= value;
-        printf("After dividing by %.2f, global variable: %.2f\n", value, globalVar);
-    } else {
-        printf("Error: Division by zero is not allowed.\n");
-    }
-}
-
-void subtractValue(float value) {
-    globalVar -= value;
-    printf("After subtracting %.2f, global variable: %.2f\n", value, globalVar);
-}
-
 int main() {
     float num;
 
@@ -42,20 +25,9 @@ int main() {
     printf("Enter a value to add: ");
     scanf("%f", &num);
     addValue(num);
-
     printf("Enter a value to multiply: ");
     scanf("%f", &num);
     multiplyValue(num);
-
-    printf("Enter a value to divide: ");
-    scanf("%f", &num);
-    divideValue(num);
-
-    printf("Enter a value to subtract: ");
-    scanf("%f", &num);
-    subtractValue(num);
-
-    printf("Final value of global variable: %.2f\n", globalVar);
-
-    return 0;
+     printf("Final value of global variable: %.2f\n", globalVar);
+return 0;
 }
